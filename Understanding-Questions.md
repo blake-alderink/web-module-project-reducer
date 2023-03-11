50f0415d6dc28258738d06077feaa4daa5c1fd26
Blake Alderink <blake.alderink@gmail.com>
2023-03-10 16:30:56 -0800

    clear button, all numbers and operators functional

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,7 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
+* dipatch runs the reduce function with the given parameter.  the addOne parameter returns the type of action which is add one.  this then returns from the reduce function state + 1 as state's new value.
 ...
 
 * TotalDisplay shows the total plus 1.
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-
+import React, { useReducer } from 'react';
+import { reducer, initialState } from "../reducers/index.js"
 import './App.css';
+import { applyNumber, changeOperation, clearDisplay } from "../actions/index"
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
+
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  console.log("this is state:", state, "this is the dispatch;", dispatch);
+
+  const clickAdd = (e) => {
+    dispatch(applyNumber(Number(e.target.value)))
+  }
+
+  const clickOperator = e => {
+    dispatch(changeOperation(e.target.value))
+  }
+
+  const clearTotal = () => {
+    dispatch(clearDisplay())
+  }
+
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -16,10 +36,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
@@ -29,31 +49,31 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={(e) => clickAdd(e)}/>
+              <CalcButton value={2} onClick={(e) => clickAdd(e)}/>
+              <CalcButton value={3} onClick={(e) => clickAdd(e)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={(e) => clickAdd(e)}/>
+              <CalcButton value={5} onClick={(e) => clickAdd(e)}/>
+              <CalcButton value={6} onClick={(e) => clickAdd(e)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={(e) => clickAdd(e)}/>
+              <CalcButton value={8} onClick={(e) => clickAdd(e)}/>
+              <CalcButton value={9} onClick={(e) => clickAdd(e)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={"+"} onClick={e => clickOperator(e)}/>
+              <CalcButton value={"*"} onClick={e => clickOperator(e)}/>
+              <CalcButton value={"-"} onClick={e => clickOperator(e)}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={() => clearTotal()}/>
             </div>
 
           </form>
